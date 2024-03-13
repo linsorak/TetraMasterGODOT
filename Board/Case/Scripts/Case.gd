07f295277cb92bb08eac64e685b59093b0a7bc95
LinSo <linsorak@hotmail.fr>
2024-03-13 23:43:08 +0100

    Split case on board

--- Board/Case/Scripts/Case.gd
+++ Board/Case/Scripts/Case.gd
@@ -1,7 +1,33 @@
 class_name Case
-extends Control
+extends Area2D
 
-var mouse_over = false
+var border_size = 2
 
 func _ready():
-	pass
+	$Panel.connect("mouse_entered", Callable(self, "_on_case_mouse_entered"))
+	$Panel.connect("mouse_exited", Callable(self, "_on_case_mouse_exited"))
+
+func set_size(case_size: Vector2) -> void:
+	$Panel.size = case_size
+	var collision_shape = $CollisionShape2D.shape as RectangleShape2D
+	if collision_shape == null:
+		collision_shape = RectangleShape2D.new()
+		$CollisionShape2D.shape = collision_shape
+		
+	collision_shape.extents = case_size / 2
+	$CollisionShape2D.position = case_size / 2 
+
+	$Panel.add_theme_stylebox_override("panel", StyleBoxFlat.new()) 
+	$Panel.get_theme_stylebox("panel").border_width_left = border_size 
+	$Panel.get_theme_stylebox("panel").border_width_top = border_size
+	$Panel.get_theme_stylebox("panel").border_width_right = border_size
+	$Panel.get_theme_stylebox("panel").border_width_bottom = border_size
+	$Panel.get_theme_stylebox("panel").border_color = Color.TRANSPARENT 
+	$Panel.get_theme_stylebox("panel").bg_color = Color.TRANSPARENT
+
+func _on_case_mouse_entered():
+	$Panel.get_theme_stylebox("panel").border_color = Color.FLORAL_WHITE 
+
+func _on_case_mouse_exited():
+	$Panel.get_theme_stylebox("panel").border_color = Color.TRANSPARENT 
+	
--- Board/Scripts/Board.gd
+++ Board/Scripts/Board.gd
@@ -1,5 +1,58 @@
 extends Node2D
+
 class_name Board
 
+var case_width = 182
+var case_height = 235
+var num_rows = 4
+var num_cols = 4
+var spacing = 6 
+
+var background_image : String = "res://Board/Sprites/board.png"
+
 func _ready():
-	pass
+	_set_board_background_img()
+	var grid_width = (case_width + spacing) * num_cols - spacing
+	var grid_height = (case_height + spacing) * num_rows - spacing 
+	var screen_size = get_viewport_rect().size
+	var start_x = (screen_size.x - grid_width) / 2
+	var start_y = (screen_size.y - grid_height) / 2
+	var base_case = load("res://Board/Case/Scenes/Case.tscn")
+	for row in range(num_rows):
+		for col in range(num_cols):
+			var case_instance = base_case.instantiate()
+			var case_position = Vector2(start_x + col * (case_width + spacing), start_y + row * (case_height + spacing))
+			case_instance.position = case_position
+			case_instance.set_size(Vector2(case_width, case_height))
+			add_child(case_instance)
+			
+func _set_board_background_img() -> void:
+	var image = Sprite2D.new()
+	var texture = load(background_image)
+	var texture_size = texture.get_size()
+	var screen_size = get_viewport_rect().size
+	
+	print("Taille de la fenêtre:", screen_size)
+	
+	var target_height = screen_size.y
+	var scale_factor = target_height / texture_size.y
+	
+	image.texture = texture
+	image.scale = Vector2(scale_factor, scale_factor)
+	
+	# Obtenez la taille de l'image après mise à l'échelle
+	var image_size = texture_size * scale_factor
+	
+	print("Taille de l'image:", image_size)
+	
+	image.position.x = (screen_size.x - image_size.x) / 2 + (image_size.x / 2)
+	image.position.y = (screen_size.y - image_size.y) / 2 + (image_size.y / 2) 
+	
+	print("Position de l'image:", image.position)
+	
+	add_child(image)
+
+
+
+
+
